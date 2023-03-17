aef12f9593e6f257400af17dd95348a10d3ded97
anggaihza <angga.asep98@gmail.com>
2023-03-18 05:29:14 +0700

    first commit

---
+++ src/pages/LearnHOC.jsx
@@ -0,0 +1,12 @@
+function learnHOC(Component) {
+  return (props) => {
+    const style = {
+      padding: "0.2rem",
+      margin: "1rem",
+      backgroundColor: "red",
+    };
+    return <Component style={style} {...props} />;
+  };
+}
+
+export default learnHOC;
---
+++ src/pages/Testing.jsx
@@ -0,0 +1,13 @@
+function Testing() {
+  const register = () => {
+    alert("Test");
+  };
+
+  return (
+    <div>
+      <button onClick={register}>Test{Testing}</button>
+    </div>
+  );
+}
+
+export default Testing;
