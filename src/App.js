cc5608e58aa29f6658a34accb2aa5d21b5ed7504
anggaihza <angga.asep98@gmail.com>
2023-03-18 05:48:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, useNavigate} from "react-router-dom";
 import Home from "./pages/Home";
 import Testing from "./pages/Testing";
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div>
+      <button
+        onClick={() => {
+          navigate("/register");
+        }}>
+        Home
+      </button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Testing />} />
